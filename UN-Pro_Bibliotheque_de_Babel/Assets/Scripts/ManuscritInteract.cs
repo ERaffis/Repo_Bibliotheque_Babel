01606c7bf3294c8f07c1d8d6d81aa4bbc64f1834
Philippe Rainville <philipperainville15@gmail.com>
2021-02-25 15:42:35 -0500

    Ajout Manuscrit du Savoir et son Menu

---
+++ UN-Pro_Bibliotheque_de_Babel/Assets/Scripts/ManuscritInteract.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManuscritInteract : MonoBehaviour
+{
+    public GameObject ManuscritMenu;
+    public GameObject TextManuscrit;
+    public bool CanInteract;
+    
+
+    private void Start()
+    {
+        CanInteract = false;
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            TextManuscrit.SetActive(true);
+            CanInteract = true;
+
+        }
+
+    }
+   
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            TextManuscrit.SetActive(false);
+            CanInteract = false;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L) && CanInteract)
+        {
+            ManuscritMenu.SetActive(true);
+           
+        }
+    }
+}
