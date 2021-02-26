a3ca6f2f1bb87b861c65f345d49a58d82f7859ba
Philippe Rainville <philipperainville15@gmail.com>
2021-02-26 14:25:07 -0500

    Ajout HUB_Principal Layout/CameraFollow/LetheSprite

---
+++ UN-Pro_Bibliotheque_de_Babel/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+  
+
+   
+    public void Update()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, -10f);        
+    }
+}
