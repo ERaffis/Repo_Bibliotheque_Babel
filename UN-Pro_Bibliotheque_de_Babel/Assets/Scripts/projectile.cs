360bb672855b2df5c92410d76f5e647f85970f99
Édouard R <edouard.raffis@umontreal.ca>
2021-03-01 13:46:24 -0500

    Merge Martin into Master V2

---
+++ UN-Pro_Bibliotheque_de_Babel/Assets/Scripts/projectile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+
+
+    // pour faire le hit effet (voir particules)
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
