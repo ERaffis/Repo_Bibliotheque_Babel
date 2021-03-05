b7ff8838ae807858a12b67ec2a914bec51a22b09
Édouard R <edouard.raffis@umontreal.ca>
2021-03-04 22:03:04 -0500

    Added Map Management for when in game and in menu

--- UN-Pro_Bibliotheque_de_Babel/Assets/Scripts/Inventory.cs
+++ UN-Pro_Bibliotheque_de_Babel/Assets/Scripts/Inventory.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class Inventory : MonoBehaviour
 {
 
+    [Header("Player")]
+    public PlayerScript player;
+
     public static Inventory instance;
+    public GameObject inventoryCanvas;
+
+    private bool menuState;
 
     public bool[] isFull;
     public GameObject[] slots;
@@ -27,14 +34,37 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuState = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OpenCloseMenu();
     }
 
+
+    void OpenCloseMenu()
+    {
+        if (player.playerInputs.GetButtonDown("Menu"))
+        {
+
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                player.playerInputs.controllers.maps.SetMapsEnabled(false, "In Game");
+                player.playerInputs.controllers.maps.SetMapsEnabled(true, "In Menu");
+            }
+            else 
+            {
+                Time.timeScale = 1;
+                player.playerInputs.controllers.maps.SetMapsEnabled(true, "In Game");
+                player.playerInputs.controllers.maps.SetMapsEnabled(false, "In Menu");   
+            }
+            
+            menuState = !menuState;
+            inventoryCanvas.SetActive(menuState);
+        }
+    }
     
 }
