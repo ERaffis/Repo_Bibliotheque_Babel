5fbd81423f45d4200ab1dc216d03c3e287ce03bd
Philippe Rainville <philipperainville15@gmail.com>
2021-03-15 15:19:18 -0400

    Ajout Prefab Projectile boss

--- UN-Pro_Bibliotheque_de_Babel/Assets/BossProjectilePrefab.cs
+++ UN-Pro_Bibliotheque_de_Babel/Assets/BossProjectilePrefab.cs
@@ -20,11 +20,7 @@ public class BossProjectilePrefab : MonoBehaviour
         damage = 12;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
